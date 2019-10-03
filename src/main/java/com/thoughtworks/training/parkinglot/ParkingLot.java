5fb0ec4b6109a5e8b3cd9d80dcdf0aae158bbdef
dipali Prakash Kelkar <kelkardipali1@gmail.com>
2019-10-03 17:00:35 +0530

    [Dipali|Avi] Add. can not park similar object

--- src/main/java/com/thoughtworks/training/parkinglot/ParkingLot.java
+++ src/main/java/com/thoughtworks/training/parkinglot/ParkingLot.java
@@ -2,15 +2,26 @@ package com.thoughtworks.training.parkinglot;
 
 public class ParkingLot {
     private int capacity;
+    private int availableSpace;
+    Object lastVehicle = new Object(); // Initialized with a dummy value
 
     public ParkingLot(int capacity) {
         this.capacity = capacity;
+        this.availableSpace = capacity;
     }
 
-    public boolean park(Object object) {
-        if (capacity == 20) {
+    public boolean park(Object nextVehicle) {
+
+
+        if (availableSpace >= capacity) {
+            availableSpace--;
+            lastVehicle = nextVehicle;
             return true;
+        }
+        if (nextVehicle.equals(lastVehicle)) {
+            throw new IllegalArgumentException("similar object can not allowed");
         }
         return false;
     }
 }
+
--- src/test/java/com/thoughtworks/training/parkinglot/ParkingLotTest.java
+++ src/test/java/com/thoughtworks/training/parkinglot/ParkingLotTest.java
@@ -7,7 +7,25 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ParkingLotTest {
     @Test
     void givenParkingLot_WhenPark_ThenShouldBePark() {
-        ParkingLot parkingLot = new ParkingLot(20);
+        ParkingLot parkingLot = new ParkingLot(2);
         assertTrue(parkingLot.park(new Object()));
     }
+
+    @Test
+    void givenParkingLot_WhenPark_ThenShouldNotPark() {
+        ParkingLot parkingLot = new ParkingLot(1);
+        parkingLot.park(new Object());
+
+        assertFalse(parkingLot.park(new Object()));
+    }
+
+    @Test
+    void givenParkingLot_WhenParkTwoSame_ThenShouldNotPark() {
+        ParkingLot parkingLot = new ParkingLot(2);
+        Object object1 = new Object();
+        parkingLot.park(object1);
+
+        assertThrows(IllegalArgumentException.class, () -> parkingLot.park(object1), "similar object can not allowed");
+    }
+
 }
